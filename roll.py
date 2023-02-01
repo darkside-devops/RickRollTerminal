f47e2b74cd9198070013d4eecc98d2e5c8047684
Aptify <121524673+OfficialCJDesigns@users.noreply.github.com>
2023-02-01 15:55:32 -0600

    Update roll.py

--- roll.py
+++ roll.py
@@ -2,25 +2,17 @@ import moviepy.editor as mp
 import numpy as np
 import subprocess
 
-# Load the video file
-video = mp.VideoFileClip("Rick roll.mp4")
+# Created by Aptify!
 
-# Extract the audio and play it in the background
-subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-i", "Rick roll.mp4"])
+video = mp.VideoFileClip("video.mp4")
 
-# Loop through each frame in the video
+subprocess.Popen(["ffplay", "-autoexit", "-nodisp", "-i", "video.mp4"])
+
 for frame in video.iter_frames(fps=24):
-    # Convert the frame to grayscale
     gray = np.dot(frame, [0.2989, 0.5870, 0.1140])
-
-    # Normalize the grayscale values to be between 0 and 255
     gray = gray.astype(np.uint8)
-
-    # Loop through each row in the frame
     for row in gray:
-        # Loop through each pixel in the row
         for pixel in row:
-            # Map the grayscale value to ASCII characters
             if pixel > 240:
                 character = " "
             elif pixel > 200:
@@ -34,6 +26,5 @@ for frame in video.iter_frames(fps=24):
             else:
                 character = "@"
 
-            # Print the ASCII character to the console
             print(character, end="")
         print()
